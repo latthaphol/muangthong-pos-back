c7aad67f8ae636a6648e84b48aba9cfea02ef639
unknown <latthaphol.s@kkumail.com>
2024-02-09 19:08:21 +0700

    ff

--- config/database.js
+++ config/database.js
@@ -1,10 +1,10 @@
 const options = {
     client: 'mysql',
     connection: {
-        host: 'localhost',
-        user: 'root',
-        password: '',
-        database: 'pos_productlot',
+        host: 'bcmzjcg2vcphd3fzqmxu-mysql.services.clever-cloud.com',
+        user: 'uyuxekmpn7v3wp1f',
+        password: 'awI0ujrUQiuK63NfDirH',
+        database: 'bcmzjcg2vcphd3fzqmxu',
         port: 3306
     }
 }
